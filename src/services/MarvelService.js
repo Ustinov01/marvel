5d504a8fd6831397c3e30c8260e44e590e9e32c5
Oleg <oleg.ustinov.1993@mail.ru>
2023-07-15 11:13:09 +0600

    fetch init

---
+++ src/services/MarvelService.js
@@ -0,0 +1,28 @@
+
+
+class MarvelService {
+
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    _apiKey = 'apikey=7681499ea8456a7a3493ca32b857a099';
+    getResource = async (url) => {
+        let res = await fetch(url);
+
+        if(!res.ok) {
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+        }
+
+
+        return await res.json();
+    }
+
+
+    getAllCharacters = () => {
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    }
+
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
+}
+
+export default MarvelService
