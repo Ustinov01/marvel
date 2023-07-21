5b9b67c3162213fe42255ab63f98d22145e9dcf4
Oleg <oleg.ustinov.1993@mail.ru>
2023-07-21 18:35:00 +0600

    errorBoundary done

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -4,6 +4,7 @@ class MarvelService {
 
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=7681499ea8456a7a3493ca32b857a099';
+    _baseOffset = 210;
     getResource = async (url) => {
         let res = await fetch(url);
 
@@ -16,8 +17,8 @@ class MarvelService {
     }
 
 
-    getAllCharacters = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter)
     }
 
@@ -39,7 +40,8 @@ class MarvelService {
             description: char.description ? (char.description.length > 150 ? char.description.slice(0, 150) + '...' : char.description) : 'description unfortunately missing',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
