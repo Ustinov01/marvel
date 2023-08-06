0d46d1bc91055b088452a971b2f60ab3511fe207
Oleg <oleg.ustinov.1993@mail.ru>
2023-08-06 17:46:47 +0600

    debug fetch comics

--- src/components/comicsList/ComicsList.js
+++ src/components/comicsList/ComicsList.js
@@ -51,7 +51,7 @@ const ComicsList = () => {
                     <a href="#">
                         <img src={item.thumbnail} alt={item.title} className="comics__item-img"/>
                         <div className="comics__item-name">{item.title}</div>
-                        {/* <div className="comics__item-price">{item.price}</div> */}
+                        <div className="comics__item-price">{item.price}</div>
                     </a>
                 </li>
             )
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -38,7 +38,7 @@ const useMarvelService = () => {
     }
 
     const getAllComics = async (offset = 0) => {
-        const res = await request(`${_apiBase}characters?limit=8&offset=${offset}&${_apiKey}`);
+        const res = await request(`${_apiBase}comics?limit=8&offset=${offset}&${_apiKey}`);
         return  res.data.results.map(_transformComics);
     }
 
@@ -58,15 +58,17 @@ const useMarvelService = () => {
     const _transformComics = (comics) => {
 		return {
 			id: comics.id,
-			title: comics[0].title,
+			title: comics.title,
 			description: comics.description || "There is no description",
 			pageCount: comics.pageCount
 				? `${comics.pageCount} p.`
 				: "No information about the number of pages",
 			thumbnail: comics.thumbnail.path + "." + comics.thumbnail.extension,
-			// price: comics.prices[0].price
-			// 	? `${comics.prices[0].price}$`
-			// 	: "not available",
+			language: comics.textObjects[0]?.language || "en-us",
+			// optional chaining operator
+			price: comics.prices[0].price
+				? `${comics.prices[0].price}$`
+				: "not available",
 		};
 	};
 
