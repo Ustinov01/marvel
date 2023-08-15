6fbdd1afc18da5303c069d074ba93fae7157d868
Oleg <oleg.ustinov.1993@mail.ru>
2023-08-15 15:46:39 +0600

    work with helmet and new build

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,10 +1,17 @@
 import ComicsList from "../comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner"
-
+import { Helmet } from "react-helmet";
 
 const ComicPage = () => {
     return (
         <>
+            <Helmet>
+                <meta
+                    name="description"
+                    content="Page with our comics"
+                />
+                <title>Comics Page</title>
+            </Helmet>
             <AppBanner/>
             <ComicsList/>  
         </>
